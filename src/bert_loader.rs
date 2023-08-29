95117936986d1532b2f3173e3bb507ac63fc8c06
Tyler Vergho <tvergho@gmail.com>
2023-08-28 23:35:36 -0400

    load from safetensors fn

--- src/bert_loader.rs
+++ src/bert_loader.rs
@@ -11,6 +11,8 @@ use crate::model::{
 };
 use std::io::Read;
 use npy::NpyData;
+use candle::{safetensors, Device, Tensor as CandleTensor};
+use std::collections::HashMap;
 
 fn load_npy_scalar<B: Backend>(filename: &str) -> Vec<f32> {
     // Open the file in read-only mode.
@@ -72,10 +74,40 @@ fn load_2d_tensor<B: Backend>(filename: &str) -> Param<Tensor<B, 2>> {
   param
 }
 
+fn load_1d_tensor_from_candle<B: Backend>(tensor: &CandleTensor) -> Tensor<B, 1> {
+  let dims = tensor.dims();
+  let data = tensor.to_vec1::<f32>().unwrap();
+  let array: [usize; 1] = dims.try_into().expect("Unexpected size");
+  let data = Data::new(data, Shape::new(array));
+  let weight = Tensor::<B, 1>::from_floats(data);
+  weight
+}
+
+fn load_2d_tensor_from_candle<B: Backend>(tensor: &CandleTensor) -> Tensor<B, 2> {
+  let dims = tensor.dims();
+  let data = tensor.to_vec2::<f32>().unwrap().into_iter().flatten().collect::<Vec<f32>>();
+  let array: [usize; 2] = dims.try_into().expect("Unexpected size");
+  let data = Data::new(data, Shape::new(array));
+  let weight = Tensor::<B, 2>::from_floats(data);
+  weight
+} 
+
 fn load_layer_norm<B: Backend>(dir: &str) -> LayerNormRecord<B> {
   let layer_norm_record = LayerNormRecord {
     beta: load_1d_tensor::<B>(&format!("{}LayerNorm_bias.npy", dir)),
     gamma: load_1d_tensor::<B>(&format!("{}LayerNorm_weight.npy", dir)),
+    epsilon: ConstantRecord::new()
+  };
+  layer_norm_record
+}
+
+fn load_layer_norm_safetensor<B: Backend>(bias: &CandleTensor, weight: &CandleTensor) -> LayerNormRecord<B> {
+  let beta = load_1d_tensor_from_candle::<B>(bias);
+  let gamma = load_1d_tensor_from_candle::<B>(weight);
+
+  let layer_norm_record = LayerNormRecord {
+    beta: beta.into(),
+    gamma: gamma.into(),
     epsilon: ConstantRecord::new()
   };
   layer_norm_record
@@ -89,10 +121,35 @@ fn load_linear<B: Backend>(dir: &str) -> LinearRecord<B> {
   linear_record
 }
 
+fn load_linear_safetensor<B: Backend>(bias: &CandleTensor, weight: &CandleTensor) -> LinearRecord<B> {
+  let bias = load_1d_tensor_from_candle::<B>(bias);
+  let weight = load_2d_tensor_from_candle::<B>(weight);
+
+  let weight = weight.transpose();
+
+  let linear_record = LinearRecord {
+    weight: weight.into(),
+    bias: Some(bias.into()),
+  };
+  linear_record
+}
+
 fn load_output_layer<B: Backend>(layer_dir: &str) -> BertOutputRecord<B> {
   let output_record = BertOutputRecord {
     dense: load_linear::<B>(&format!("{}dense/", layer_dir)),
     layer_norm: load_layer_norm::<B>(&format!("{}", layer_dir)),
+    dropout: ConstantRecord::new()
+  };
+  output_record
+}
+
+fn load_output_layer_safetensor<B: Backend>(dense_weight: &CandleTensor, dense_bias: &CandleTensor, layer_norm_bias: &CandleTensor, layer_norm_weight: &CandleTensor) -> BertOutputRecord<B> {
+  let dense = load_linear_safetensor::<B>(dense_bias, dense_weight);
+  let layer_norm = load_layer_norm_safetensor::<B>(layer_norm_bias, layer_norm_weight);
+
+  let output_record = BertOutputRecord {
+    dense,
+    layer_norm,
     dropout: ConstantRecord::new()
   };
   output_record
@@ -106,11 +163,39 @@ fn load_intermediate_layer<B: Backend>(layer_dir: &str) -> BertIntermediateRecor
   intermediate_record
 }
 
+fn load_intermediate_layer_safetensor<B: Backend>(dense_weight: &CandleTensor, dense_bias: &CandleTensor) -> BertIntermediateRecord<B> {
+  let dense = load_linear_safetensor::<B>(dense_bias, dense_weight);
+
+  let intermediate_record = BertIntermediateRecord {
+    dense,
+    intermediate_act: ConstantRecord::new(),
+  };
+  
+  intermediate_record
+}
+
 fn load_self_attention_layer<B: Backend>(layer_dir: &str) -> BertSelfAttentionRecord<B> {
   let attention_record = BertSelfAttentionRecord {
     query: load_linear::<B>(&format!("{}query/", layer_dir)),
     key: load_linear::<B>(&format!("{}key/", layer_dir)),
     value: load_linear::<B>(&format!("{}value/", layer_dir)),
+    dropout: ConstantRecord::new(),
+    num_attention_heads: ConstantRecord::new(),
+    attention_head_size: ConstantRecord::new(),
+    all_head_size: ConstantRecord::new(),
+  };
+  attention_record
+}
+
+fn load_self_attention_layer_safetensor<B: Backend>(query_weight: &CandleTensor, query_bias: &CandleTensor, key_weight: &CandleTensor, key_bias: &CandleTensor, value_weight: &CandleTensor, value_bias: &CandleTensor) -> BertSelfAttentionRecord<B> {
+  let query = load_linear_safetensor::<B>(query_bias, query_weight);
+  let key = load_linear_safetensor::<B>(key_bias, key_weight);
+  let value = load_linear_safetensor::<B>(value_bias, value_weight);
+
+  let attention_record = BertSelfAttentionRecord {
+    query,
+    key,
+    value,
     dropout: ConstantRecord::new(),
     num_attention_heads: ConstantRecord::new(),
     attention_head_size: ConstantRecord::new(),
@@ -128,10 +213,33 @@ fn load_self_output_layer<B: Backend>(layer_dir: &str) -> BertSelfOutputRecord<B
   output_record
 }
 
+fn load_self_output_layer_safetensor<B: Backend>(dense_weight: &CandleTensor, dense_bias: &CandleTensor, layer_norm_bias: &CandleTensor, layer_norm_weight: &CandleTensor) -> BertSelfOutputRecord<B> {
+  let dense = load_linear_safetensor::<B>(dense_bias, dense_weight);
+  let layer_norm = load_layer_norm_safetensor::<B>(layer_norm_bias, layer_norm_weight);
+
+  let output_record = BertSelfOutputRecord {
+    dense,
+    layer_norm,
+    dropout: ConstantRecord::new()
+  };
+  output_record
+}
+
 fn load_attention_layer<B: Backend>(layer_dir: &str) -> BertAttentionRecord<B> {
   let attention_record = BertAttentionRecord {
     self_attention: load_self_attention_layer::<B>(&format!("{}self/", layer_dir)),
     self_output: load_self_output_layer::<B>(&format!("{}output/", layer_dir)),
+  };
+  attention_record
+}
+
+fn load_attention_layer_safetensor<B: Backend>(attention_tensors: HashMap<String, CandleTensor>) -> BertAttentionRecord<B> {
+  let self_attention = load_self_attention_layer_safetensor::<B>(&attention_tensors["attention.self.query.weight"], &attention_tensors["attention.self.query.bias"], &attention_tensors["attention.self.key.weight"], &attention_tensors["attention.self.key.bias"], &attention_tensors["attention.self.value.weight"], &attention_tensors["attention.self.value.bias"]);
+  let self_output = load_self_output_layer_safetensor::<B>(&attention_tensors["attention.output.dense.weight"], &attention_tensors["attention.output.dense.bias"], &attention_tensors["attention.output.LayerNorm.bias"], &attention_tensors["attention.output.LayerNorm.weight"]);
+
+  let attention_record = BertAttentionRecord {
+    self_attention,
+    self_output,
   };
   attention_record
 }
@@ -166,9 +274,62 @@ fn load_encoder<B: Backend>(encoder_dir: &str) -> BertEncoderRecord<B> {
   encoder_record
 }
 
+fn load_encoder_from_safetensors<B: Backend>(encoder_tensors: HashMap<String, CandleTensor>) -> BertEncoderRecord<B> {
+  // Each layer in encoder_tensors has a key like encoder.layer.0, encoder.layer.1, etc.
+  // We need to extract the layers in order by iterating over the tensors and extracting the layer number
+  let mut layers: HashMap<usize, HashMap<String, CandleTensor>> = HashMap::new();
+
+  for (key, value) in encoder_tensors.iter() {
+    let layer_number = key.split(".").collect::<Vec<&str>>()[2].parse::<usize>().unwrap();
+    if !layers.contains_key(&layer_number) {
+      layers.insert(layer_number, HashMap::new());
+    }
+    layers.get_mut(&layer_number).unwrap().insert(key.to_string(), value.clone());
+  }
+
+  // Sort layers.iter() by key
+  let mut layers = layers.into_iter().collect::<Vec<(usize, HashMap<String, CandleTensor>)>>();
+  layers.sort_by(|a, b| a.0.cmp(&b.0));
+
+  // Now, we can iterate over the layers and load each layer
+  let mut bert_encoder_layers: Vec<BertEncoderLayerRecord<B>> = Vec::new();
+  for (key, value) in layers.iter() {
+    let layer_key = format!("encoder.layer.{}", key.to_string());
+    let attention_tensors = value.clone();
+    // Remove the layer number from the key
+    let attention_tensors = attention_tensors.iter().map(|(k, v)| (k.replace(&format!("{}.", layer_key), ""), v.clone())).collect::<HashMap<String, CandleTensor>>();
+    let attention_layer = load_attention_layer_safetensor::<B>(attention_tensors.clone());
+    let intermediate_layer = load_intermediate_layer_safetensor::<B>(&value[&format!("{}.intermediate.dense.weight", layer_key)], &value[&format!("{}.intermediate.dense.bias", layer_key)]);
+    let output_layer = load_output_layer_safetensor::<B>(&value[&format!("{}.output.dense.weight", layer_key)], &value[&format!("{}.output.dense.bias", layer_key)], &value[&format!("{}.output.LayerNorm.bias", layer_key)], &value[&format!("{}.output.LayerNorm.weight", layer_key)]);
+
+    let layer_record = BertEncoderLayerRecord {
+      attention: attention_layer,
+      intermediate: intermediate_layer,
+      output: output_layer,
+    };
+
+    bert_encoder_layers.push(layer_record);
+  }
+
+  let encoder_record = BertEncoderRecord {
+    layers: bert_encoder_layers,
+  };
+  encoder_record
+}
+
 fn load_embedding<B: Backend>(embedding_dir: &str) -> EmbeddingRecord<B> {
   let embedding = EmbeddingRecord {
     weight: load_2d_tensor::<B>(&format!("{}weight.npy", embedding_dir)),
+  };
+
+  embedding
+}
+
+fn load_embedding_safetensor<B: Backend>(weight: &CandleTensor) -> EmbeddingRecord<B> {
+  let weight = load_2d_tensor_from_candle(weight);
+
+  let embedding = EmbeddingRecord {
+    weight: weight.into()
   };
 
   embedding
@@ -193,6 +354,25 @@ fn load_embeddings<B: Backend>(embeddings_dir: &str) -> BertEmbeddingsRecord<B> 
   embeddings_record
 }
 
+fn load_embeddings_from_safetensors<B: Backend>(embedding_tensors: HashMap<String, CandleTensor>) -> BertEmbeddingsRecord<B> {
+  let word_embeddings = load_embedding_safetensor(&embedding_tensors["embeddings.word_embeddings.weight"]);
+  let position_embeddings = load_embedding_safetensor(&embedding_tensors["embeddings.position_embeddings.weight"]);
+  let token_type_embeddings = load_embedding_safetensor(&embedding_tensors["embeddings.token_type_embeddings.weight"]);
+  let layer_norm = load_layer_norm_safetensor::<B>(&embedding_tensors["embeddings.LayerNorm.bias"], &embedding_tensors["embeddings.LayerNorm.weight"]);
+  let dropout = ConstantRecord::new();
+
+  let embeddings_record = BertEmbeddingsRecord {
+    word_embeddings,
+    position_embeddings,
+    token_type_embeddings,
+    layer_norm,
+    dropout,
+    max_position_embeddings: ConstantRecord::new(),
+  };
+
+  embeddings_record
+}
+
 pub fn load_model<B: Backend>(dir: &str, device: &B::Device, config: BertModelConfig) -> BertModel<B> {
   let encoder_record = load_encoder::<B>(&format!("{}/encoder/", dir));
   let embeddings_record = load_embeddings::<B>(&format!("{}/embeddings/", dir));
@@ -200,6 +380,43 @@ pub fn load_model<B: Backend>(dir: &str, device: &B::Device, config: BertModelCo
   let model_record = BertModelRecord {
       embeddings: embeddings_record,
       encoder: encoder_record,
+  };
+
+  let mut model = config.init_with::<B>(model_record);
+
+  model = model.to_device(device);
+  model
+}
+
+pub fn load_model_from_safetensors<B: Backend>(file_path: &str, device: &B::Device, config: BertModelConfig) -> BertModel<B> {
+  let file_path = std::path::Path::new(file_path);
+  let weight_result = safetensors::load::<&std::path::Path>(file_path, &Device::Cpu);
+
+  // Match on the result of loading the weights
+  let weights = match weight_result {
+    Ok(weights) => weights,
+    Err(e) => panic!("Error loading weights: {:?}", e),
+  };
+
+  // Weights are stored in a HashMap<String, Tensor>
+  // For each layer, it will either be prefixed with "encoder.layer." or "embeddings."
+  // We need to extract both.
+  let mut encoder_layers: HashMap<String, CandleTensor> = HashMap::new();
+  let mut embeddings_layers: HashMap<String, CandleTensor> = HashMap::new();
+
+  for (key, value) in weights.iter() {
+    if key.starts_with("encoder.layer.") {
+      encoder_layers.insert(key.to_string(), value.clone());
+    } else if key.starts_with("embeddings.") {
+      embeddings_layers.insert(key.to_string(), value.clone());
+    }
+  }
+  
+  let embeddings_record = load_embeddings_from_safetensors::<B>(embeddings_layers);
+  let encoder_record = load_encoder_from_safetensors::<B>(encoder_layers);
+  let model_record = BertModelRecord {
+    embeddings: embeddings_record,
+    encoder: encoder_record,
   };
 
   let mut model = config.init_with::<B>(model_record);
--- tests/load_and_compare.rs
+++ tests/load_and_compare.rs
@@ -1,6 +1,6 @@
 extern crate sentence_transformers;
 
-use sentence_transformers::bert_loader::load_model;
+use sentence_transformers::bert_loader::{load_model, load_model_from_safetensors};
 use sentence_transformers::model::bert_embeddings::BertEmbeddingsInferenceBatch;
 use sentence_transformers::model::bert_model::{BertModel, BertModelConfig};
 use burn_tch::{TchDevice, TchBackend};
@@ -46,9 +46,62 @@ fn compare_dump_model_outputs() {
   };
 
   let model: BertModel<TchBackend<f32>> = load_model((path.clone() + "/model").as_str(), &device, config);
+
   let input_ids: Tensor<TchBackend<f32>, 1> = Tensor::from_floats([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]).to_device(&device.clone());
   let attn_mask: Tensor<TchBackend<f32>, 1> = Tensor::from_floats([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]).to_device(&device.clone());
+  let input_ids = input_ids.int().reshape(Shape::new([1, 10])); 
+  let attn_mask = attn_mask.reshape(Shape::new([1, 10]));
 
+  println!("input_ids: {:?}", input_ids.clone().to_data().value);
+  println!("attn_mask: {:?}", attn_mask.clone().to_data().value);
+  
+  let input = BertEmbeddingsInferenceBatch {
+    tokens: input_ids,
+    mask_attn: Some(attn_mask),
+  };
+
+  let output = model.forward(input);
+  let output_vec: Vec<f32> = output.to_data().value.to_vec();
+
+  // Compare with outputs from outputs/ directory
+  let output_path = path + "/outputs/outputs_0.npy";
+  let mut buf = vec![];
+    std::fs::File::open(output_path).unwrap()
+        .read_to_end(&mut buf).unwrap();
+
+  let data: NpyData<f32> = NpyData::from_bytes(&buf).unwrap();
+  let data_vec: Vec<f32> = data.to_vec();
+
+  // Compare length of each
+  assert_eq!(output_vec.len(), data_vec.len());
+
+  // Compare each value
+  for i in 0..output_vec.len() {
+    assert!((output_vec[i] - data_vec[i]).abs() < 0.0001);
+  }
+}
+
+#[test]
+fn compare_safetensors_model_outputs() {
+  let path = setup();
+  let device = TchDevice::Cpu;
+  let config = BertModelConfig { 
+    n_heads: 6, 
+    n_layers: 6, 
+    layer_norm_eps: 1e-12, 
+    hidden_size: 192, 
+    intermediate_size: 768, 
+    hidden_act: "gelu".to_string(),
+    vocab_size: 30522, 
+    max_position_embeddings: 512, 
+    type_vocab_size: 2, 
+    hidden_dropout_prob: 0.1 
+  };
+
+  let model: BertModel<TchBackend<f32>> = load_model_from_safetensors::<TchBackend<f32>>((path.clone() + "/model/bert_model.safetensors").as_str(), &device, config.clone());
+  
+  let input_ids: Tensor<TchBackend<f32>, 1> = Tensor::from_floats([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]).to_device(&device.clone());
+  let attn_mask: Tensor<TchBackend<f32>, 1> = Tensor::from_floats([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]).to_device(&device.clone());
   let input_ids = input_ids.int().reshape(Shape::new([1, 10])); 
   let attn_mask = attn_mask.reshape(Shape::new([1, 10]));
 
