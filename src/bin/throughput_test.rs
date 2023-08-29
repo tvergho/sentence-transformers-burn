685c715616830a9c0c25b097dc92d0e080a71f66
Tyler Vergho <tvergho@gmail.com>
2023-08-29 00:34:20 -0400

    updated throughput test with gpu

--- src/bin/throughput_test.rs
+++ src/bin/throughput_test.rs
@@ -5,17 +5,21 @@ use sentence_transformers::model::{
 use burn::tensor::Tensor;
 use sentence_transformers::bert_loader::{load_model_from_safetensors, load_config_from_json};
 use burn_tch::{TchBackend, TchDevice};
+use burn_wgpu::{AutoGraphicsApi, WgpuBackend, WgpuDevice};
 use std::env;
 
-const BATCH_SIZE: u64 = 128;
+const BATCH_SIZE: u64 = 64;
 
 fn main() {
-  let device = TchDevice::Mps;
+  // let device = TchDevice::Mps;
+  let device: WgpuDevice = WgpuDevice::BestAvailable;
+
   let args: Vec<String> = env::args().collect();
   let model_path = args.get(1).expect("Expected model directory as first argument");
 
   let config = load_config_from_json(&format!("{}/bert_config.json", model_path));
-  let model: BertModel<_> = load_model_from_safetensors::<TchBackend<f32>>(&format!("{}/bert_model.safetensors", model_path), &device, config);
+  // let model: BertModel<_> = load_model_from_safetensors::<TchBackend<f32>>(&format!("{}/bert_model.safetensors", model_path), &device, config);
+  let model: BertModel<_> = load_model_from_safetensors::<WgpuBackend<AutoGraphicsApi, f32, i32>>(&format!("{}/bert_model.safetensors", model_path), &device, config);
 
   loop {
     let start = std::time::Instant::now();
@@ -28,6 +32,5 @@ fn main() {
     model.forward(batch);
     
     println!("{:?}", start.elapsed());
-    std::thread::sleep(std::time::Duration::from_millis(1000));
   }
 }
